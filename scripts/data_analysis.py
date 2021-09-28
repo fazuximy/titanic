4a1e99a9805ff6f1458e8f7bba7d3647f7f79c8a
fazuximy <fazuximy12@gmail.com>
2021-09-28 22:11:56 +0200

    Adding empty python scripts

---
+++ scripts/data_analysis.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 21:43:41 2021
+
+@author: Fazuximy
+"""
+
---
+++ scripts/prediction_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 21:44:34 2021
+
+@author: Fazuximy
+"""
+
